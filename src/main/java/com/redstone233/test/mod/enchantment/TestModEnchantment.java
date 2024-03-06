06ae4e0a31b53c81383dc7026a787ecb74d27c25
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-06 22:54:17 +0800

    Fixed some files

--- src/main/java/com/redstone233/test/mod/enchantment/TestModEnchantment.java
+++ src/main/java/com/redstone233/test/mod/enchantment/TestModEnchantment.java
@@ -6,7 +6,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 
-public class TestModEnchantment extends Enchantment{
+public class TestModEnchantment extends Enchantment {
 
     public TestModEnchantment(Rarity rarity, EnchantmentTarget target, EquipmentSlot[] slotTypes) {
         super(rarity, target, slotTypes);
@@ -21,4 +21,3 @@ public class TestModEnchantment extends Enchantment{
 
         super.onTargetDamaged(user, target, level);
     }
-}
