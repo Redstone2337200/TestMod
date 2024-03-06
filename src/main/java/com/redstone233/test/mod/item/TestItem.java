85c1e61f91b43431ecea307f0b225036c047a216
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-06 20:49:46 +0800

    Create TestItem.java

---
+++ src/main/java/com/redstone233/test/mod/item/TestItem.java
@@ -0,0 +1,31 @@
+package com.redstone233.test.mod.item;
+
+import java.util.List;
+
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+public class TestItem extends Item {
+    public TestItem(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        user.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
+        return TypedActionResult.success(user.getStackInHand(hand));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+        //super.appendTooltip(stack, world, tooltip, context);
+        tooltip.add(Text.translatable("item.testmod.test_item"));
+    }
+}
