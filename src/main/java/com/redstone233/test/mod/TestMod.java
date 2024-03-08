a5fc72456538ec759b1e1491bac377198724a3f5
Redstone2337200 <100214029+Redstone2337200@users.noreply.github.com>
2024-03-08 13:38:10 +0800

    Add files via upload

--- src/main/java/com/redstone233/test/mod/TestMod.java
+++ src/main/java/com/redstone233/test/mod/TestMod.java
@@ -1,23 +1,30 @@
 package com.redstone233.test.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.render.fluid.v1.FluidRenderHandlerRegistry;
+import net.fabricmc.fabric.api.client.render.fluid.v1.SimpleFluidRenderHandler;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.block.FluidBlock;
 import net.minecraft.block.MapColor;
 import net.minecraft.block.enums.Instrument;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.fluid.FlowableFluid;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.SwordItem;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -34,6 +41,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.redstone233.test.mod.armor.TestArmorMaterial;
 import com.redstone233.test.mod.block.TestBlock;
 import com.redstone233.test.mod.enchantment.TestModEnchantment;
+import com.redstone233.test.mod.fluid.TestWaterFluid;
 import com.redstone233.test.mod.item.TestItem;
 import com.redstone233.test.mod.tool.TestToolMaterial;
 
@@ -88,6 +96,22 @@ public class TestMod implements ModInitializer {
 					EquipmentSlot.OFFHAND
 			}));
 
+	public static final FlowableFluid STILL_TEST_WATER = 
+		Registry.register(Registries.FLUID, new Identifier("testmod", "test_water"),
+			new TestWaterFluid.Still());
+			
+	public static final FlowableFluid FLOWABLE_TEST_WATER = 
+		Registry.register(Registries.FLUID, new Identifier("testmod", "flowing_test_water"), 
+			new TestWaterFluid.Flowing());
+			
+	public static final Item TEST_WATER_BUCKET =
+		Registry.register(Registries.ITEM, new Identifier("testmod", "test_water_bucket"), 
+			new BucketItem(STILL_TEST_WATER, new FabricItemSettings().recipeRemainder(Items.BUCKET).maxCount(1)));
+
+	public static final Block TEST_WATER = 
+		Registry.register(Registries.BLOCK, new Identifier("testmod","test_water"), 
+			new FluidBlock(STILL_TEST_WATER, FabricBlockSettings.copy(Blocks.WATER)));
+
 	@SuppressWarnings("unused")
 	private static final ItemGroup ITEM_GROUP = Registry.register(Registries.ITEM_GROUP,
 			new Identifier("testmod", "item_group"),
@@ -113,11 +137,18 @@ public class TestMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 		LOGGER.info("Hello Fabric world!");
+		FluidRenderHandlerRegistry.INSTANCE.register(STILL_TEST_WATER,FLOWABLE_TEST_WATER,
+			new SimpleFluidRenderHandler(
+				new Identifier("minecraft:block/water_still"), 
+				new Identifier("minecraft:block/water_flow"),
+					0x730D95
+				));
+
 		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, enviroment) -> register(dispatcher));
 	}
 
 	private Object register(CommandDispatcher<ServerCommandSource> dispatcher) {
-		throw new UnsupportedOperationException("Unimplemented method 'register'");
+		return dispatcher;
 	}
 
 }
